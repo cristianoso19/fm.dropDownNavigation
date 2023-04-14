2a5cdd5be3dd6ea1c2daf8e2e16605164acc317a
CristianLenovoU22 <cristianoso19@gmail.com>
2023-04-14 00:40:36 -0500

    Make mobile layout of challenge

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       fontFamily: {
         sans: ["Epilogue", "sans-serif"],
       },
+      backgroundImage: {
+        'hero-mobile': "url('../images/image-hero-mobile.png')",
+        'hero-dektop': "url('../images/image-hero-desktop.png')",
+      }
+    },
+    colors: {
+      'almostWhite': 'hsl(0, 0%, 91%)',
+      'mediumGray': 'hsl(0, 0%, 41%)',
+      'AlmostBlack': 'hsl(0, 0%, 8%)',
     },
   },
   plugins: [],
