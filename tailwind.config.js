6b942162a8753be936ceb8eeb97ea9bab618430a
CristianLenovoU22 <cristianoso19@gmail.com>
2023-04-23 23:22:14 -0500

    Desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    screens: {
+      'sm': '576px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '960px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '1440px',
+      // => @media (min-width: 1440px) { ... }
+    },
     extend: {
       fontFamily: {
         sans: ["Epilogue", "sans-serif"],
